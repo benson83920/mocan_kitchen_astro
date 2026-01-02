8e86b5e804668678f90f8e8e3839d877ae64df90
benson83920 <benson83092@gmail.com>
2026-01-02 18:21:22 +0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://mocan-kitchen.github.io',
+    base: '/mocan_kitchen_astro',
 });
