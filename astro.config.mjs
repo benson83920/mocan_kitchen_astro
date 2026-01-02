ca81c63b54323ccf241cb5580c39a642d58779c0
benson83920 <benson83092@gmail.com>
2026-01-02 18:24:37 +0700

    feat

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mocan-kitchen.github.io',
-    base: '/mocan_kitchen_astro',
+    base: '/mocan-kitchen-astro',
 });
